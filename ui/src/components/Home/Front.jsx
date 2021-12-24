713f13e504f39d56dc3610451e00344d96c8544b
xena <1runx3na2@gmail.com>
2021-12-24 23:32:46 +0530

    Front maintextcontainer

--- ui/src/components/Home/Front.jsx
+++ ui/src/components/Home/Front.jsx
@@ -3,15 +3,49 @@ import styled from "styled-components";
 
 const Container = styled.div`
   margin-top: 9vh;
-
+  height: 90vh;
 `;
 
 const Wrapper = styled.div`
+    height: 100%;
     display: flex;
     justify-content: space-around;
+    padding: 10px 10px;
 `;
 
-const Left = styled.div``;
+const Left = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MainTextContainer = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+`;
+
+const FirstText = styled.h1`
+  /* font-size: 50px;
+  font-weight: 500; */
+`;
+const SecondText = styled.div``;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const ExploreButton = styled.div``;
+const CreateButton = styled.div``;
+const LearnMoreContainer = styled.div`
+  display: flex;
+`;
+const LearnIcon = styled.div``;
+const LearnText = styled.div``;
+
 
 const Right = styled.div``;
 
@@ -19,7 +53,20 @@ export const Front = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>left</Left>
+        <Left>
+          <MainTextContainer>
+            <FirstText>Discover, collect, and sell extraordinary NFTs</FirstText>
+            <SecondText>ArtBytes is the world's first and largest NFT marketplace</SecondText>
+            <ButtonContainer>
+              <ExploreButton>explore</ExploreButton>
+              <CreateButton>create</CreateButton>
+            </ButtonContainer>
+          </MainTextContainer>
+          <LearnMoreContainer>
+            <LearnIcon>icon</LearnIcon>
+            <LearnText>Learn more about ArtBytes</LearnText>
+          </LearnMoreContainer>
+        </Left>
         <Right>right</Right>
       </Wrapper>
     </Container>
