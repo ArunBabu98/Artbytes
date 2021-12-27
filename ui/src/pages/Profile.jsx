00cb6f3886490e11a5d2325900cb1946f0d7132e
xena <1runx3na2@gmail.com>
2021-12-27 19:30:23 +0530

    Profile page

--- ui/src/pages/Profile.jsx
+++ ui/src/pages/Profile.jsx
@@ -2,14 +2,89 @@ import React from 'react'
 import styled from 'styled-components'
 import Navbar from '../components/Home/Navbar';
 
-const Container = styled.div``;
-
-const ProfileContainer =styled.div`
-    background-color: green;
-    height: 50vh;
+const Container = styled.div`
+    margin-top: 10vh;
 `;
 
-const = styled.div``;
+const ProfileContainer =styled.div`
+    height: 30vh;
+    display: flex;
+    justify-content: center;
+`;
+
+const FirstContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: 90%;
+    height: 80%;
+`;
+const Propic = styled.div`
+    background-color: green;
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+    margin: 10px 40px;
+`;
+const DescContainer = styled.div`
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    margin-right: 20px;
+`;
+const Name = styled.div`
+    font-size: 40px;
+    font-weight: 500;
+    margin-bottom: 10px;
+`;
+
+const UsernameContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+const Username = styled.div`
+    color: #39B290;
+    font-weight: 600;
+`;
+
+const Verified = styled.div`
+margin-left: 5px;
+    height: 15px;
+    width: 15px;
+    border-radius: 50%;
+    background-color: #39B290;
+`;
+
+const Description = styled.div`
+    font-weight: 400;
+    margin-bottom: 10px;
+`;
+
+const ReadMoreContainer = styled.div`
+    display: flex;
+`;
+const Readmore = styled.div`
+    font-weight: 600;
+`;
+const ReadmoreIcon = styled.div``;
+
+const StatsConatainer = styled.div`
+    margin-left: 300px;
+`;
+
+const FollowersContainer = styled.div``;
+const Followers = styled.div``;
+const FollowerStat = styled.div``;
+const FollowingContainer = styled.div``;
+const Following = styled.div``;
+const FollowingStat = styled.div``;
+const ForsaleContainer = styled.div``;
+const Forsale = styled.div``;
+const ForsaleStat = styled.div``;
+const SecondContainer = styled.div``;
+const IconsContainer = styled.div``;
+const ButtonContainer = styled.div``;
 
 const Profile = () => {
     return (
